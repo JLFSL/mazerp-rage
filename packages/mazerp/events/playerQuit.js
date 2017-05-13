96e55a8f07e37114dbc827cee0a9651336a54ba5
Jimmy Lelkes <jlfslelkes@gmail.com>
2017-05-13 20:19:17 +0200

    Removed the personal message on playerQuit ;)

--- packages/mazerp/events/playerQuit.js
+++ packages/mazerp/events/playerQuit.js
@@ -4,7 +4,6 @@ const sendGlobalMessage = require("../functions/sendGlobalMessage");
 
 module.exports = {
 	"playerQuit": (player) => {
-		sendPlayerMessage(player, "Welcome to the MazeRP Development Server!");
 		sendGlobalMessage(`${player.name} has left.`);
 	}
 };

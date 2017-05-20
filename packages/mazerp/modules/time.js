365b4425767fc68b026ba0560b384afefa4eef29
Svvite <HannoGrimm@yahoo.de>
2017-05-20 17:08:34 +0200

    time.js adjustments + House  Class prototype

--- packages/mazerp/modules/time.js
+++ packages/mazerp/modules/time.js
@@ -1,29 +1,29 @@
 var time = 21600, // Start the clock at 6am
-    minutes = 0;
+    ticks = 0;
 
 mp.environment.time.hour = Math.floor(time / (60 * 60));
 mp.environment.time.minute = Math.floor((time % (60 * 60)) / 60);
 mp.environment.time.second = Math.floor((time % (60 * 60) / 60) % 60);
 
-// console.log(mp.environment.time);
-// console.log(minutes);
+console.log(mp.environment.time);
+console.log(ticks);
 
 setInterval(() => {
-    minutes += 1;
-    if (minutes >= 60) {
-        minutes = 0; 
+    ticks += 1;
+    if (ticks >= 360000) {
+        ticks = 0; 
         time = 21600;
     }
     
-    if (minutes <= 45) time += 1120;
-    else time += 2400;
+    if (ticks <= 270000) time += 0.18667;
+    else time += 0.40;
     
     if (time >= 86400) time -= 86400;
 
     mp.environment.time.hour = Math.floor(time / (60 * 60));
     mp.environment.time.minute = Math.floor((time % (60 * 60)) / 60);
-    mp.environment.time.second = Math.floor((time % (60 * 60) / 60) % 60);
+    mp.environment.time.second = Math.floor(time % 60);
 
-    // console.log(mp.environment.time);
-    // console.log(minutes);
-}, 30000);
+    console.log(mp.environment.time);
+    console.log(ticks);
+}, 10);
